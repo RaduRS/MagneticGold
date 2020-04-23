19d43da65a4c37c5065041856bc25e2a21895431
RaduRS <rsrusu90@gmail.com>
2020-04-23 11:00:39 +0100

    new changes

--- Assets/Scripts/GoldNugget.cs
+++ Assets/Scripts/GoldNugget.cs
@@ -3,10 +3,26 @@
 public class GoldNugget : MonoBehaviour
 {
     public Rigidbody rb;
+    public int count = 1;
+    public bool isActive = true;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         rb.AddForce(Physics.gravity * 2f, ForceMode.Acceleration);
+
+        if (isActive == false)
+        {
+            count = 0;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Magnet")
+        {
+            isActive = false;
+            count = 0;
+        }
     }
 }
--- Assets/Scripts/Magnet.cs
+++ Assets/Scripts/Magnet.cs
@@ -1,21 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Magnet : MonoBehaviour
 {
-
     public float moveSpeed = 5f;
     private GoldNugget goldScript;
+    public float maxHold = 0f;
 
+    public MagnetPoint margnetPointScript;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
- 
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
@@ -23,17 +21,25 @@ public class Magnet : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
 
         transform.Translate(movement * Time.deltaTime * moveSpeed);
+
+        if (maxHold >= 6f)
+        {
+            margnetPointScript = gameObject.GetComponent<MagnetPoint>();
+            margnetPointScript.forceFactor = 0f;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Gold")
+        if (collision.gameObject.tag == "Gold")
         {
             goldScript = collision.gameObject.GetComponent<GoldNugget>();
 
             //goldScript.rb.constraints = RigidbodyConstraints.FreezeAll;
 
+            maxHold = maxHold + goldScript.count;
+            goldScript.isActive = false;
+
         }
-        
     }
 }
